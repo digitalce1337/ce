eea455196e7c65b25ada856906a51689527dfb04
Siti Zulaiha <sitizulaiha.m@digipen.edu>
2019-06-17 15:29:23 +0800

    remove duplicated from master

--- ionic_koreanapp/src/app/apiurls/serverurls.js
+++
@@ -1,7 +0,0 @@
-//Production on AWS - Public DNS
-//export const apiKey = 'http://18.222.185.105/'
-
-//Testing on local pc - Jeremy IPv4 address
-//export const apiKey = 'http://192.168.8.100:8000/'
-
-export const apiKey = 'http://18.216.233.84/'
