1db60048bda694756568e16adc2c23b70f25f238
Siti Zulaiha <sitizulaiha.m@digipen.edu>
2019-06-19 14:01:53 +0800

    hehe job info

--- ionic4/src/app/pages/owner-job-info/owner-job-info.page.ts
+++ ionic4/src/app/pages/owner-job-info/owner-job-info.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-owner-job-info',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OwnerJobInfoPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  editJob(){
+    console.log("editing");
+    // this.nav.push(EditjobPage, {'jid':this.jid, 'access_token':this.access_token});
+    // let navigationExtras: NavigationExtras = {
+    //   queryParams: {
+    //       token: this.access_token,
+    //       id: this.jid
+    //   }
+    // };
+    // this.navCtrl.navigateForward('/EditjobPage', navigationExtras);
+    this.navCtrl.navigateForward('/owner-edit-job');
+    // this.nav.push(EditjobPage, {'access_token':this.access_token,'jid':this.jid });
   }
 
 }
