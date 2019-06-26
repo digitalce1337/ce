60298534f91df375ef1ba5e94f4c85596dd1dc97
Siti Zulaiha <sitizulaiha.m@digipen.edu>
2019-06-26 11:37:15 +0800

    26/6/19 side menu for all tab pages

--- ionic4/src/app/app.component.ts
+++ ionic4/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AuthService } from './services/auth.service';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 
 import {TranslateService} from '@ngx-translate/core'
 import { LanguageService } from './services/language.service';
@@ -16,11 +16,11 @@ import { LanguageService } from './services/language.service';
 export class AppComponent {
 
   public appPages = [
-    {
-      title: 'Home',
-      url: '/owner/tabs/owner-home',
-      icon: 'home'
-    },
+    // {
+    //   title: 'Home',
+    //   url: '/owner/tabs/owner-home',
+    //   icon: 'home'
+    // },
     {
       title: 'Profile',
       url: '/profile-details',
@@ -28,14 +28,19 @@ export class AppComponent {
     }
   ];
 
+  public toggle: boolean = false;
+  public Role: boolean = true;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     public authService: AuthService,
     public router: Router,
+    public navCtrl: NavController,
     private _translate : TranslateService,
-    private languageService: LanguageService
+    private languageService: LanguageService,
+    public menu: MenuController
   ) {
     this.initializeApp();
   }
@@ -57,5 +62,46 @@ export class AppComponent {
         }
       })
     });
+  }
+
+  checkRole_() {
+    if(this.Role == true) {
+      this.toggleProfile();
+    }
+    else {
+      return;
+    }
+  }
+
+  toggleProfile(){
+    let navToggle: NavigationExtras = {
+      queryParams: {
+        toggled: this.toggle
+      }
+    }
+
+    if(this.toggle == true){
+      // this.nav.setRoot(OperatorstabsPage);
+      this.navCtrl.navigateRoot(['operator/tabs/operator-home']);
+      // this.navCtrl.navigateForward(['operator/tabs/operator-home'], navToggle);
+
+      this.menu.close();
+    }
+    else{
+      // this.nav.setRoot(TabsPage);
+      this.navCtrl.navigateRoot(['owner/tabs/owner-home']);
+      // this.navCtrl.navigateForward(['owner/tabs/owner-home'], navToggle);
+      this.menu.close();
+    }
+  }
+
+  openPage(page){
+    let navToggle: NavigationExtras = {
+      queryParams: {
+        toggled: this.toggle
+      }
+    }
+    // this.nav.setRoot(page.pageName, {toggled: this.toggleButton});
+    this.navCtrl.navigateForward(page, navToggle);
   }
 }
--- ionic4/src/app/pages/profile-details/profile-details.page.ts
+++ ionic4/src/app/pages/profile-details/profile-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile-details',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileDetailsPage implements OnInit {
 
-  constructor() { }
+  public toggled;
+
+  constructor(public navCtrl: NavController, public activeRoute: ActivatedRoute) {
+    this.activeRoute.queryParams.subscribe(params => {
+      this.toggled = params["toggled"];
+      console.log("toggle status: " + this.toggled);
+    });
+   }
 
   ngOnInit() {
+    this.popToHome();
+  }
+
+  popToHome(){
+    // if(this.Role == 1 && this.toggled == true) {
+    //   this.navCtrl.push(OperatorstabsPage);  
+    // }
+    // else if(this.Role ==1 && this.toggled == false) {
+    //   this.navCtrl.push(TabsPage)
+    // }
+    // else {
+    //   this.navCtrl.push(OperatorstabsPage);
+    // }
+    if(this.toggled == false) { 
+      // this.navCtrl.push(TabsPage);
+      this.navCtrl.navigateBack(['owner/tabs/owner-home']);
+    }
+    else if(this.toggled == true) {
+      // this.navCtrl.push(OperatorstabsPage);
+      this.navCtrl.navigateBack(['operator/tabs/operator-home']);
+    }
+
+    else {
+      // this.navCtrl.push(OperatorstabsPage);
+      this.navCtrl.navigateBack(['operator/tabs/operator-home']);
+    }
   }
 
 }
