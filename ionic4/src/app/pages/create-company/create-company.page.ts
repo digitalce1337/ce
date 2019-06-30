8f07ebc7bf85638cc9cc012e290688c8803d3714
Siti Zulaiha <sitizulaiha.m@digipen.edu>
2019-06-30 19:36:44 +0800

    30/6/19 create company & profile updated

--- ionic4/src/app/pages/create-company/create-company.page.ts
+++ ionic4/src/app/pages/create-company/create-company.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AlertController, NavController } from '@ionic/angular';
+import { AppService } from 'src/app/services/app.service';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-create-company',
@@ -7,9 +12,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCompanyPage implements OnInit {
 
-  constructor() { }
+  public language: string;
+  public title: string;
+  public company_name: string;
+  public company_address: string;
+  public phone_no: string;
+  public working_days: string = '30';
+  public continue: string;
+
+  private access_token: string;
+  companynameField: string = "";
+  companyaddField: string = "";
+  phonenoField: string = "";
+  workingdaysField: string = "30";
+
+
+  constructor(public _translate: TranslateService, public alertCtrl: AlertController, public storage: Storage,
+    public appprov: AppService, public router: Router, public navCtrl: NavController) { }
 
   ngOnInit() {
+    this.storage.ready().then(() => {
+      console.log("Storage ready passed");
+      this.storage.get('access_token').then((val) => {
+        console.log("Storage get passed");
+        console.log("Value: " + val + " AT: " + this.access_token);
+        this.access_token = val;
+        console.log("Set AT, check: " + this.access_token);
+        this._translateLanguage();      
+      })
+    });
+  }
+
+  public changeLanguage(): void {
+    this._translateLanguage();
+  }
+
+  private _translateLanguage(): void {
+    this._translate.use(this.language);
+    this._initializeTranslation();
+  }
+
+  private _initializeTranslation(): void {
+    this.title = this._translate.instant("createcompany.title");
+    this.company_name = this._translate.instant("createcompany.company_name");
+    this.company_address = this._translate.instant("createcompany.company_address");
+    this.phone_no = this._translate.instant("createcompany.phone_no");
+    this.working_days = this._translate.instant("createcompany.working_days");
+    this.continue = this._translate.instant("createcompany.continue");
+  }
+
+  async emptyAlert() {
+    let alert = await this.alertCtrl.create({
+      header: 'Empty Field(s)',
+      subHeader: 'Please fill up all details before proceeding',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  itemTappedAdd() {
+    // this.navCtrl.setRoot(TabsPage);
+    let company_name = this.companynameField;
+    let company_add = this.companyaddField;
+    let phone_no = this.phonenoField;
+    let working_days = this.workingdaysField;
+
+    // if (company_name == "" || company_add == "" || phone_no == "" || working_days == "") {
+    if (company_name == "" || company_add == "" || phone_no == "") {
+      this.emptyAlert();
+      return;
+    }
+
+    this.appprov.addCompany(this.access_token, company_name, company_add, phone_no, working_days).then((res) => {
+      let data = JSON.stringify(res);
+      data = JSON.parse(data);
+      if (data['result'].toString() == 'true') {
+        console.log("Company and phone number added");
+      }
+      // this.nav.setRoot(TabsPage);
+      this.router.navigateByUrl('owner/tabs/owner-home');
+
+
+    }, err => {
+      console.log(err);
+    });
+    //this.navCtrl.setRoot(TabsPage);
+
   }
 
 }
--- ionic4/src/app/pages/profile-details/profile-details.page.ts
+++ ionic4/src/app/pages/profile-details/profile-details.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, Events } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-profile-details',
@@ -8,14 +12,50 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile-details.page.scss'],
 })
 export class ProfileDetailsPage implements OnInit {
+  public title: string;
+
+  public companyName: string;
+  public companyAdd: string;
+  public email: string;
+  public phone_no: string;
+  public language: string;
+
+  public result: any;
+  public userinfo: any;
+
+  public Uname: any;
+  public Udob: any;
+  public Uemail: any;
+  public uimg: any;
+  public Ucompany: any;
+  public Ucompanyadd: any;
+  public Unum: any;
+
+  profile: any;
+
+  private access_token: string;
+  loading: any;
 
   public toggle;
   public Role: boolean;
 
-  constructor(public navCtrl: NavController, public activeRoute: ActivatedRoute,
-    public event: Events) {}
+  constructor(public navCtrl: NavController, public activeRoute: ActivatedRoute, public storage: Storage, public appprov: AppService,
+    public event: Events, public _translate: TranslateService, public authService: AuthService) { }
 
   ngOnInit() {
+    this.storage.ready().then(() => {
+      console.log("Storage ready passed");
+      this.storage.get('access_token').then((val) => {
+        console.log("Storage get passed");
+        console.log("Value: " + val + " AT: " + this.access_token);
+        this.access_token = val;
+        console.log("Set AT, check: " + this.access_token);
+        this._translateLanguage();
+        this.getUserInfo();
+      })
+    });
+
+    //get value for toggle value & user role
     this.event.subscribe('toggleValue', (value) => {
       console.log("toggleValue: " + value);
       this.toggle = value;
@@ -24,21 +64,38 @@ export class ProfileDetailsPage implements OnInit {
       console.log("role value: " + res);
       this.Role = res;
     });
-    // this.popToHome();
   }
 
-  popToHome(){
+  public changeLanguage(): void {
+    this._translateLanguage();
+  }
+
+  private _translateLanguage(): void {
+    this._translate.use(this.language);
+    this._initializeTranslation();
+  }
+
+  private _initializeTranslation(): void {
+    this.title = this._translate.instant("profile.title");
+    this.companyName = this._translate.instant("profile.companyName");
+    this.companyAdd = this._translate.instant("profile.companyAdd");
+    this.email = this._translate.instant("profile.email");
+    this.phone_no = this._translate.instant("profile.phone_no");
+
+  }
+
+  popToHome() {
     console.log("poptoHome this.toggled:" + this.toggle);
     console.log("poptoHome this.Role:" + this.Role);
     // console.log("Give result: "+ this.Role);
 
-    if(this.Role == true && this.toggle == false) { 
-    // if(this.toggle == false) { 
+    if (this.Role == true && this.toggle == false) {
+      // if(this.toggle == false) { 
       // this.navCtrl.push(TabsPage);
       this.navCtrl.navigateBack(['owner/tabs/owner-home']);
     }
-    else if(this.Role == true && this.toggle == true) {
-    // else if(this.toggle == true) {
+    else if (this.Role == true && this.toggle == true) {
+      // else if(this.toggle == true) {
       // this.navCtrl.push(OperatorstabsPage);
       this.navCtrl.navigateBack(['operator/tabs/operator-home']);
     }
@@ -47,6 +104,23 @@ export class ProfileDetailsPage implements OnInit {
       // this.navCtrl.push(OperatorstabsPage);
       this.navCtrl.navigateBack(['operator/tabs/operator-home']);
     }
+  }
+
+  getUserInfo() {
+    this.authService.getUserinfo(this.access_token).then((result) => {
+      this.userinfo = result;
+      // this.userinfo = JSON.parse(this.userinfo._body);
+      this.Uemail = this.userinfo.email;
+      this.Udob = this.userinfo.dob;
+      this.Uname = this.userinfo.name;
+      this.uimg = this.userinfo.profile_image_url;
+      this.Ucompany = this.userinfo.company_name;
+      this.Ucompanyadd = this.userinfo.company_add;
+      this.Unum = this.userinfo.phone_no;
+      this.appprov.setemail(this.userinfo.email);
+    }, (err) => {
+      console.log(err);
+    });
   }
 
 }
